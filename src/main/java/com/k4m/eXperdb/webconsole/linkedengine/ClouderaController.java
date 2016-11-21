68e613ba1e3daf50908104a19d4c5e1e18d336df
manimany <javara@dreamwiz.com>
2016-11-21 13:27:01 +0900

    패스워드 디코딩 로직 추가

--- src/main/java/com/k4m/eXperdb/webconsole/linkedengine/ClouderaController.java
+++ src/main/java/com/k4m/eXperdb/webconsole/linkedengine/ClouderaController.java
@@ -68,7 +68,7 @@ public class ClouderaController {
 				
 				//데이터베이스에서 조회된 값으로 REST URL을 만들고, 해당 URL로 조회 요청을 하고 결과값을 반환 받는다.
 				//TODO 패스워드 디코드 로직 추가
-				clouderaServiceSummary = clouderaServiceSummary((String)serverInfo.get("ip"),(String) serverInfo.get("port"), (String)serverInfo.get("user_id"),SecureManager.decrypt(((String)serverInfo.get("user_pw"))));
+				clouderaServiceSummary = clouderaServiceSummary((String)serverInfo.get("ip"),(String) serverInfo.get("port"), (String)serverInfo.get("user_id"),SecureManager.decrypt(((String)serverInfo.get("user_pw")))); 
 				//clouderaServiceSummary = clouderaServiceSummary((String)serverInfo.get("ip"),(String) serverInfo.get("port"), (String)serverInfo.get("user_id"),"admin");
 				serverInfo.put("services", clouderaServiceSummary.get("services"));
 				serverInfo.put("servicesCount", clouderaServiceSummary.get("servicesCount"));		
